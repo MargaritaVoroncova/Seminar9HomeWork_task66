e5dcd03d3a8dc35bfdf4597ebbe040997bb28a9a
MargaritaVoroncova <margaritavrncv@gmail.com>
2022-09-04 10:08:51 +0300

    task66

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задайте значения M и N. Напишите программу,
+ которая найдёт сумму чётных чисел натуральных элементов 
+ в промежутке от M до N.*/
+
+ Console.WriteLine("Введите число M: ");
+int firstUserNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int secondUserNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstUserNumber % 2 != 0)
+{
+    firstUserNumber++; 
+}
+
+int NumbersRec(int firstUserNumber, int secondUserNumber)
+{
+    if(firstUserNumber <= secondUserNumber)
+    { 
+        return firstUserNumber + NumbersRec(firstUserNumber + 2, secondUserNumber);
+    }
+    else
+    { 
+         return 0;
+    }
+}
+
+Console.WriteLine(NumbersRec(firstUserNumber, secondUserNumber));
